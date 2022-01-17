5eb546d5d5f00119da177f9acd0ada902c43d6ca
Mario Gómez <magv181091@gmail.com>
2022-01-16 22:00:25 -0500

    Added header and first section

---
+++ src/components/Header.jsx
@@ -0,0 +1,75 @@
+
+// Libraries
+import { React, useState } from 'react'
+import { Navbar, Nav, Container, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import Offcanvas from 'react-bootstrap/Offcanvas'
+
+// Media
+import dress from '../static/img/dress.svg';
+import user from '../static/img/user.svg';
+import cart from '../static/img/cart.svg';
+
+// CSS
+import '../static/css/Main.css';
+import '../static/css/Header.css';
+
+export default function Header() {
+
+    function ShoppingCart({ name, ...props }) {
+        const [show, setShow] = useState(false);
+        const [items, setItems] = useState([]);
+      
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+      
+        return (
+          <>
+            <Nav.Link onClick={handleShow}>
+              {name}
+            </Nav.Link>
+            <Offcanvas show={show} onHide={handleClose} {...props}>
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Shopping cart</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                Your cart is empty.
+              </Offcanvas.Body>
+            </Offcanvas>
+          </>
+        );
+    }
+
+    return (
+        <> 
+            <Navbar expand="lg" style={{ backgroundColor: "#E0D7C7" }}>
+                <Container>
+                    <Navbar.Brand href="#home" className="logoFont">
+                        <img alt="" src={dress} width="30" height="30" className="d-inline-block align-top" />{' '}
+                        Shoppingly
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto links">
+                            <Nav.Link href="#">Men</Nav.Link>
+                            <Nav.Link href="#">Women</Nav.Link>
+                            <Nav.Link href="#">Kids</Nav.Link>
+                            <Nav.Link href="#">Offers</Nav.Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <FormControl type="search" placeholder="Something in mind?" className="me-2" aria-label="Search" />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                        <Nav.Link href="#">
+                            <img alt="" src={user} width="30" height="30" className="d-inline-block align-top header-icons" />
+                        </Nav.Link>
+                        <Navbar.Brand href="#home" className="links">
+                            {['end'].map((placement, idx) => (
+                                <ShoppingCart key={idx} placement={placement} name={<img alt="" src={cart} width="30" height="30" className="d-inline-block align-top header-icons" />} />
+                            ))}
+                        </Navbar.Brand>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
---
+++ src/components/Prueba.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+
+export default function Prueba() {
+
+    const [count, setCount] = useState(0);
+    const [counter, setCounter] = useState(0);
+   
+
+    useEffect(() => {
+        document.title = `You have clicked the buttons ${counter + 1} times`; 
+    });
+
+
+    return (
+        <div>
+            <h1>Hello, have a look a this button:</h1>
+            <h2>Counter:{count}</h2>
+            <button onClick={() => (setCount(count + 1), setCounter(counter +1))}>+</button>
+            <button onClick={() => (setCount(count - 1), setCounter(counter +1))}>-</button>
+            <h2>{document.title}</h2>
+        </div>
+    )
+}
