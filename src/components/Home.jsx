551374200679351c8f5dd75564ebe78abb9294bb
Mario Gómez <magv181091@gmail.com>
2022-01-21 11:40:24 -0500

    Changes in Home component, css file and added pictures

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,10 +7,17 @@ import React from 'react'
 import '../static/css/Main.css';
 import '../static/css/Home.css';
 
+
+// Media
+import casualFall from '../static/img/stock-photos/women/casual-fall.jpg';
+import casualBlue from '../static/img/stock-photos/women/casual-blue.jpg';
+import blueJeanJacket from '../static/img/stock-photos/women/bluejean-jacket.jpg';
+import forest from '../static/img/stock-photos/women/forest.jpg';
+
 export default function Home() {
     return (
         <>
-            <section>
+            <section style={{ backgroundColor: "white" }}>
                 <div className="discount-section">
                     <h3 className="discount-header header2">Up to </h3>
                     <h1 className="discount-header header1"> <strong> 50% </strong></h1>
@@ -19,14 +26,28 @@ export default function Home() {
                     <button className="discount-btn" style={{ color: "#56453E" }}><h6 className="header4">Learn more</h6></button>
                     <br />
                     <h5 className="discount-header header3">*Valid only for references for the selected event tag. Promos are subject to each store's availability 
-                                                            and are valid for both online and physical stores. Promos are subject to terms and conditions.</h5>
+                                                            and are valid for both online and physical stores. Subject to terms and conditions.</h5>
                 </div>
             </section>
-            <section>
-                <div className="womens-wear">
-
+            <container className="womens-wear">
+                <div className="womens-wear-items pic1">
+                    <img className="grid-ww-pic" src={forest} alt="" />
                 </div>
-            </section>
+                <div className="womens-wear-items pic2">
+                    <img className="grid-ww-pic" src={casualFall} alt="" />
+                    <div className="img-info">
+                        <h1 className="img-title" style={{ color: "#E0D7C7" }}>Latest Women's Wear</h1>
+                        <button className="img-btn">See more</button>
+                    </div>
+                </div>
+                <div className="womens-wear-items pic3">
+                    <img className="grid-ww-pic" src={blueJeanJacket} alt="" />
+                </div>
+            </container>
+            <div className="offers">
+                <h1 className="img-title" style={{ color: "#E0D7C7" }}>Latest Women's Wear</h1>
+                <button className="img-btn">See more</button>
+            </div>
         </>
     )
 }
